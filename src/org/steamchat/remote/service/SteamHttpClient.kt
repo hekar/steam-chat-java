f812ecc1e5b099b9f65863ea0a2dd3c6470a6320
Hekar <hekark@gmail.com>
2012-12-25 15:32:34 -0500

    implemented http get/post

--- src/org/steamchat/remote/service/SteamHttpClient.kt
+++ src/org/steamchat/remote/service/SteamHttpClient.kt
@@ -1,12 +1,22 @@
 package org.steamchat.remote.service
 
 import org.steamchat.model.user.*
+import org.apache.http.impl.client.*
+import org.apache.http.client.methods.*
 
 private class SteamHttpClient(val hostname: String, val port: Int) {
-    fun get(account: Account, path: String): Unit {
 
+    fun get(account: Account, path: String): String? {
+        val client = DefaultHttpClient()
+        val h = HttpGet(hostname + path)
+
+        return client.execute(h, BasicResponseHandler())
     }
 
-    fun post(account: Account, path: String, content: String): Unit {
+    fun post(account: Account, path: String, content: String): String? {
+        val client = DefaultHttpClient()
+        val h = HttpPost(hostname + path)
+
+        return client.execute(h, BasicResponseHandler())
     }
 }
